6b77061f66f0b0c62398f34aeabbef192422bd51
saud maruli <uthrul@gmail.com>
2018-09-17 10:51:39 +0700

    add bar

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -4,13 +4,20 @@ void main(){
   runApp(
     MaterialApp(
       title: "Hello Flutter App",
-      home: new Material(
-        color: Colors.deepOrange,
-        child: Center(child: new Text(
-          "hello flutter",
-          textDirection: TextDirection.ltr,
-        ),),
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text("Title in App Bar"),
+        ),
+        body: Material(
+          color: Colors.deepOrange,
+          child: Center(
+            child: new Text(
+            "hello flutter",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 30.0 ),
+          ),
+        ),
       ),
-    ) 
-  );
+    )
+  ));
 }
